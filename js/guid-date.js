6de4c17693ac29589268645d2e4ebdc60786dbdc
chen <3445409497@qq.com>
2022-02-26 10:53:00 +0800

    'asdf'

---
+++ js/guid-date.js
@@ -0,0 +1,10 @@
+
+
+var xin=document.getElementsByClassName('qwe')[0]
+
+xin.onclick=function(){
+    this.firstElementChild.setAttribute("src","../img/xinRedh.png");
+    this.nextElementSibling.firstElementChild.innerHTML=16
+}
+
+
